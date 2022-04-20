05c4d2f24314c3fbbd10a4bc73b10362f8667fb2
Hallo89 <hallo89@protonmail.com>
2022-04-20 16:34:57 +0200

    Reorganize canvas code into new class ScratchetCanvas

---
+++ static/script/ScratchetCanvas.js
@@ -0,0 +1,226 @@
+class ScratchetCanvas {
+  canvas;
+  ctx;
+
+  pressedMouseBtn = -1;
+  globalPosBuffer = new Set(); // Set<posDataWrapperInDrawOrder>
+  posUserCache = new Map(); // Map<userID, Set<posDataWrapperForUser>>
+  lastPos = new Array(2);
+  posBuffer = new Array();
+
+  constructor(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+
+    canvas.addEventListener('contextmenu', this.canvasContext.bind(this));
+    canvas.addEventListener('pointerdown', this.canvasDown.bind(this));
+    window.addEventListener('pointerup', this.pointerUp.bind(this));
+    canvas.addEventListener('mousemove', this.canvasDraw.bind(this));
+
+    canvas.height = canvas.clientHeight;
+    canvas.width = canvas.clientWidth;
+
+    this.ctx.lineCap = 'round';
+    this.ctx.lineJoin = 'round';
+
+    this.setStrokeStyle();
+    this.setLineWidth();
+  }
+
+  // ---- Event functions ----
+  canvasContext(e) {
+    if (e.button === 2) {
+      e.preventDefault();
+    }
+  }
+
+  canvasDown(e) {
+    this.pressedMouseBtn = e.button;
+    if (this.pressedMouseBtn === 2) {
+      toggleDrawIndicatorEraseMode();
+    }
+    this.setLastPos(e.clientX, e.clientY);
+    this.resetPosBuffer();
+    this.canvasDraw(e);
+  }
+
+  pointerUp() {
+    sendPositions();
+    this.pressedMouseBtn = -1;
+    toggleDrawIndicatorEraseMode(true);
+  }
+
+  canvasDraw(e) {
+    moveDrawIndicator(e.clientX, e.clientY);
+    if (this.pressedMouseBtn >= 0) {
+      sendPositionsIfWidthHasChanged();
+
+      if (this.pressedMouseBtn === 2) {
+        this.erasePos(e.clientX, e.clientY, CURRENT_USER_ID);
+        this.redrawCanvas();
+      } else {
+        sendPositionsIfHueHasChanged();
+
+        this.ctx.beginPath();
+        this.ctx.moveTo(...this.lastPos);
+        this.ctx.lineTo(e.clientX, e.clientY);
+        this.ctx.stroke();
+
+        this.setLastPos(e.clientX, e.clientY);
+      }
+      this.posBuffer.push(e.clientX, e.clientY);
+    }
+  }
+
+  // ---- Canvas handling ----
+  redrawCanvas() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    for (const posDataWrapper of this.globalPosBuffer) {
+      for (const posData of posDataWrapper) {
+        this.drawFromPosData(posData);
+      }
+    }
+  }
+
+  clearCurrentUserCanvas() {
+    sendMessage('clearUser');
+    this.clearUserBufferAndRedraw(CURRENT_USER_ID);
+  }
+
+  drawFromPosData(posData) {
+    this.setStrokeStyle(posData[0]);
+    this.setLineWidth(posData[1]);
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(posData[2], posData[3]);
+    for (let i = 4; i < posData.length; i += 2) {
+      this.ctx.lineTo(posData[i], posData[i + 1]);
+    }
+    this.ctx.stroke();
+
+    this.setStrokeStyle();
+    this.setLineWidth();
+  }
+  addPosDataToBufferAndDraw(posData, userID) {
+    this.addPosDataToBuffer(posData, userID);
+    this.drawFromPosData(posData);
+  }
+
+  // ---- Pos buffer ----
+  setLastPos(posX, posY) {
+    this.lastPos[0] = posX;
+    this.lastPos[1] = posY;
+  }
+  resetPosBuffer() {
+    if (this.pressedMouseBtn === 2) {
+      this.posBuffer = [-2, widthSlider.value];
+    } else {
+      this.posBuffer = [hueSlider.value, widthSlider.value, ...this.lastPos];
+    }
+  }
+
+  // ---- Buffer functions ----
+  handleBulkInitData(data, userID) {
+    let index = 1;
+    for (let i = 1; i < data.length; i++) {
+      if (data[i] === -1) {
+        this.addPosDataToBufferAndDraw(data.subarray(index, i), userID);
+        index = i + 1;
+      }
+    }
+    this.addPosDataToBufferAndDraw(data.subarray(index), userID);
+  }
+  handleEraseData(data, userID) {
+    if (this.posUserCache.has(userID)) {
+      const userPosSet = this.posUserCache.get(userID);
+      for (let i = 2; i < data.length; i += 2) {
+        this.erasePos(data[i], data[i + 1], userID, userPosSet, data[1]);
+      }
+      this.redrawCanvas();
+    }
+  }
+  erasePos(posX, posY, userID, userPosSet, eraserWidth = widthSlider.value) {
+    if (!userPosSet) {
+      if (!this.posUserCache.has(userID)) return;
+      userPosSet = this.posUserCache.get(userID);
+    }
+    for (const posDataWrapper of userPosSet) {
+
+      for (let i = 0; i < posDataWrapper.length; i++) {
+        const posData = posDataWrapper[i];
+
+        let newPosData = [posData[0], posData[1], posData[2], posData[3]];
+        for (let j = 4; j < posData.length; j += 2) {
+          // Push only the points back into the array which are not in range of the erase pos
+          if (Math.abs(posData[j] - posX) > eraserWidth || Math.abs(posData[j + 1] - posY) > eraserWidth) {
+            newPosData.push(posData[j], posData[j + 1]);
+          } else {
+            if (newPosData.length > 4) {
+              posDataWrapper.push(new Int32Array(newPosData));
+            }
+            if (j <= posData.length - 4) {
+              newPosData = [posData[0], posData[1], posData[j + 2], posData[j + 3]];
+            } else {
+              newPosData = [];
+            }
+          }
+        }
+
+        if (newPosData.length > 4) {
+          posDataWrapper[i] = new Int32Array(newPosData);
+        } else {
+          posDataWrapper.splice(i, 1);
+        }
+      }
+      if (posDataWrapper.length === 0) {
+        this.globalPosBuffer.delete(posDataWrapper);
+        this.posUserCache.get(userID).delete(posDataWrapper);
+      }
+    }
+  }
+
+  addPosDataToBuffer(posData, userID) {
+    const posDataWrapper = createPosDataWrapper(posData);
+    this.globalPosBuffer.add(posDataWrapper);
+    let cache = this.posUserCache.get(userID);
+    if (!cache) {
+      cache = new Set();
+      this.posUserCache.set(userID, cache);
+    }
+    cache.add(posDataWrapper);
+  }
+
+  sendJoinedUserBuffer(targetUserID) {
+    if (this.posUserCache.has(CURRENT_USER_ID)) {
+      const joinedBuffer = new Array();
+      for (const posDataWrapper of this.posUserCache.get(CURRENT_USER_ID)) {
+        for (const posData of posDataWrapper) {
+          joinedBuffer.push(-1, ...posData);
+        }
+      }
+      sock.send(new Int32Array(joinedBuffer));
+    }
+  }
+
+  clearUserBufferAndRedraw(userID) {
+    const userCache = this.posUserCache.get(userID);
+    if (userCache) {
+      for (const posDataWrapper of userCache) {
+        this.globalPosBuffer.delete(posDataWrapper);
+      }
+      userCache.clear();
+    }
+    this.redrawCanvas();
+  }
+
+  // ---- Helper functions ----
+  setLineWidth(width = widthSlider.value) {
+    this.ctx.lineWidth = width;
+    if (width === widthSlider.value) {
+      document.documentElement.style.setProperty('--strokeWidth', width + 'px');
+    }
+  }
+  setStrokeStyle(hue = hueSlider.value) {
+    this.ctx.strokeStyle = makeHSLString(hue);
+  }
+}
--- static/script/init.js
+++ static/script/init.js
@@ -5,7 +5,6 @@ const notificationTemplate = (function() {
   return node;
 }());
 
-const canvas = document.getElementById('canvas');
 const notificationWrapper = document.getElementById('notification-overlay');
 const drawIndicator = document.getElementById('draw-indicator');
 
@@ -16,14 +15,8 @@ const userListButton = document.getElementById('show-users-button');
 const CURRENT_USER_ID = -1;
 const SEND_INTERVAL = 100;
 
-const ctx = canvas.getContext('2d');
 const userListNodes = new Map();
-const posUserCache = new Map(); // Map<userID, Set<posDataWrapperForUser>>
-const globalPosBuffer = new Set(); // Set<posDataWrapperInDrawOrder>
-const lastPos = new Array(2);
 let defaultUsername;
-let pressedMouseBtn = -1;
-let posBuffer = new Array();
 
 /*
  * data/socketData: bulk data received via socket
@@ -43,7 +36,6 @@ const hueSlider = new Slider89(document.getElementById('hue-slider'), {
     </thumb>
   `
 }, true);
-hueSlider.addEvent('change:value', () => setStrokeStyle());
 
 const widthSlider = new Slider89(document.getElementById('width-slider'), {
   range: [1, 80],
@@ -55,17 +47,8 @@ const widthSlider = new Slider89(document.getElementById('width-slider'), {
     </thumb>
   `
 }, true);
-widthSlider.addEvent('change:value', () => setLineWidth());
-// Mist.
-// widthSlider.addEvent('move', (slider, e) => {
-//   if (e.ctrlKey) {
-//     if (slider.step === false) {
-//       slider.step = 10;
-//     }
-//   } else {
-//     slider.step = false;
-//   }
-// });
+
+const mainCanvas = new ScratchetCanvas(document.getElementById('canvas'));
 
 
 usernameInput.addEventListener('blur', e => {
@@ -75,26 +58,14 @@ for (const l of document.querySelectorAll('.overlay-input')) {
   l.addEventListener('keydown', handleOverlayInputKeys);
 }
 
-document.getElementById('clear-button').addEventListener('click', clearCurrentUserCanvas);
+hueSlider.addEvent('change:value', () => mainCanvas.setStrokeStyle());
+widthSlider.addEvent('change:value', () => mainCanvas.setLineWidth());
+document.getElementById('clear-button').addEventListener('click', mainCanvas.clearCurrentUserCanvas);
 
 sock.addEventListener('open', socketOpen);
 sock.addEventListener('message', socketReceiveMessage);
 
-canvas.addEventListener('contextmenu', canvasContext);
-
-canvas.addEventListener('pointerdown', canvasDown);
-window.addEventListener('pointerup', pointerUp);
-canvas.addEventListener('mousemove', canvasDraw);
 window.addEventListener('wheel', mouseWheel);
-
-
-canvas.height = canvas.clientHeight;
-canvas.width = canvas.clientWidth;
-
-ctx.lineCap = 'round';
-ctx.lineJoin = 'round';
-setStrokeStyle();
-setLineWidth();
 
 setInterval(sendPositions, SEND_INTERVAL);
 
@@ -121,110 +92,7 @@ function mouseWheel(e) {
   }
 }
 
-function canvasContext(e) {
-  if (e.button === 2) {
-    e.preventDefault();
-  }
-}
-
-function canvasDown(e) {
-  pressedMouseBtn = e.button;
-  if (pressedMouseBtn === 2) {
-    toggleDrawIndicatorEraseMode();
-  }
-  setLastPos(e.clientX, e.clientY);
-  resetPosBuffer();
-  canvasDraw(e);
-}
-
-function pointerUp() {
-  sendPositions();
-  pressedMouseBtn = -1;
-  toggleDrawIndicatorEraseMode(true);
-}
-
-function canvasDraw(e) {
-  moveDrawIndicator(e.clientX, e.clientY);
-  if (pressedMouseBtn >= 0) {
-    sendPositionsIfWidthHasChanged();
-
-    if (pressedMouseBtn === 2) {
-      erasePos(e.clientX, e.clientY, CURRENT_USER_ID);
-      redrawCanvas();
-    } else {
-      sendPositionsIfHueHasChanged();
-
-      ctx.beginPath();
-      ctx.moveTo(...lastPos);
-      ctx.lineTo(e.clientX, e.clientY);
-      ctx.stroke();
-
-      setLastPos(e.clientX, e.clientY);
-    }
-    posBuffer.push(e.clientX, e.clientY);
-
-    // hueSlider.value = (hueSlider.value + 1) % 360;
-    // if (Math.round(Math.random()) === 0 || widthSlider.value <= 1) {
-    //   widthSlider.value++;
-    // } else {
-    //   widthSlider.value--;
-    // }
-  }
-}
-
-// ---- Canvas ----
-function redrawCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  for (const posDataWrapper of globalPosBuffer) {
-    for (const posData of posDataWrapper) {
-      drawFromPosData(posData);
-    }
-  }
-}
-
-function clearCurrentUserCanvas() {
-  sendMessage('clearUser');
-  clearUserBufferAndRedraw(CURRENT_USER_ID);
-}
-
-function drawFromPosData(posData) {
-  setStrokeStyle(posData[0]);
-  setLineWidth(posData[1]);
-
-  ctx.beginPath();
-  ctx.moveTo(posData[2], posData[3]);
-  for (let i = 4; i < posData.length; i += 2) {
-    ctx.lineTo(posData[i], posData[i + 1]);
-  }
-  ctx.stroke();
-
-  setStrokeStyle();
-  setLineWidth();
-}
-function addPosDataToBufferAndDraw(posData, userID) {
-  addPosDataToBuffer(posData, userID);
-  drawFromPosData(posData);
-}
-
-// --- Canvas helper functions ---
-function setLineWidth(width = widthSlider.value) {
-  ctx.lineWidth = width;
-  if (width === widthSlider.value) {
-    document.documentElement.style.setProperty('--strokeWidth', width + 'px');
-  }
-}
-function setStrokeStyle(hue = hueSlider.value) {
-  ctx.strokeStyle = makeHSLString(hue);
-}
-function makeHSLString(hue = hueSlider.value) {
-  return `hsl(${hue}, 75%, 70%)`
-}
-
-// -- Draw indicator --
-function moveDrawIndicator(posX, posY) {
-  document.documentElement.style.setProperty('--mouseX', posX + 'px');
-  document.documentElement.style.setProperty('--mouseY', posY + 'px');
-}
+// ---- Draw indicator ----
 function toggleDrawIndicatorEraseMode(reset) {
   if (reset) {
     drawIndicator.classList.remove('erase');
@@ -232,123 +100,21 @@ function toggleDrawIndicatorEraseMode(reset) {
     drawIndicator.classList.add('erase');
   }
 }
-
-// -- Pos buffer --
-function setLastPos(posX, posY) {
-  lastPos[0] = posX;
-  lastPos[1] = posY;
+function moveDrawIndicator(posX, posY) {
+  document.documentElement.style.setProperty('--mouseX', posX + 'px');
+  document.documentElement.style.setProperty('--mouseY', posY + 'px');
 }
-function resetPosBuffer() {
-  if (pressedMouseBtn === 2) {
-    posBuffer = [-2, widthSlider.value];
-  } else {
-    posBuffer = [hueSlider.value, widthSlider.value, ...lastPos];
-  }
-}
-
 
 // ---- Buffer functions ----
 function parseSocketData(data, userID) {
   if (data[0] === -1) {
     // Bulk init data
-    handleBulkInitData(data, userID);
+    mainCanvas.handleBulkInitData(data, userID);
   } else if (data[0] === -2) {
     // Erased data
-    handleEraseData(data, userID);
+    mainCanvas.handleEraseData(data, userID);
   } else {
-    addPosDataToBufferAndDraw(data, userID);
-  }
-}
-function handleBulkInitData(data, userID) {
-  let index = 1;
-  for (let i = 1; i < data.length; i++) {
-    if (data[i] === -1) {
-      addPosDataToBufferAndDraw(data.subarray(index, i), userID);
-      index = i + 1;
-    }
-  }
-  addPosDataToBufferAndDraw(data.subarray(index), userID);
-}
-function handleEraseData(data, userID) {
-  if (posUserCache.has(userID)) {
-    const userPosSet = posUserCache.get(userID);
-    for (let i = 2; i < data.length; i += 2) {
-      erasePos(data[i], data[i + 1], userID, userPosSet, data[1]);
-    }
-    redrawCanvas();
-  }
-}
-function erasePos(posX, posY, userID, userPosSet, eraserWidth = widthSlider.value) {
-  if (!userPosSet) {
-    if (!posUserCache.has(userID)) return;
-    userPosSet = posUserCache.get(userID);
-  }
-  for (const posDataWrapper of userPosSet) {
-
-    for (let i = 0; i < posDataWrapper.length; i++) {
-      const posData = posDataWrapper[i];
-
-      let newPosData = [posData[0], posData[1], posData[2], posData[3]];
-      for (let j = 4; j < posData.length; j += 2) {
-        // Push only the points back into the array which are not in range of the erase pos
-        if (Math.abs(posData[j] - posX) > eraserWidth || Math.abs(posData[j + 1] - posY) > eraserWidth) {
-          newPosData.push(posData[j], posData[j + 1]);
-        } else {
-          if (newPosData.length > 4) {
-            posDataWrapper.push(new Int32Array(newPosData));
-          }
-          if (j <= posData.length - 4) {
-            newPosData = [posData[0], posData[1], posData[j + 2], posData[j + 3]];
-          } else {
-            newPosData = [];
-          }
-        }
-      }
-
-      if (newPosData.length > 4) {
-        posDataWrapper[i] = new Int32Array(newPosData);
-      } else {
-        posDataWrapper.splice(i, 1);
-      }
-    }
-    if (posDataWrapper.length === 0) {
-      globalPosBuffer.delete(posDataWrapper);
-      posUserCache.get(userID).delete(posDataWrapper);
-    }
-  }
-}
-
-function addPosDataToBuffer(posData, userID) {
-  const posDataWrapper = createPosDataWrapper(posData);
-  globalPosBuffer.add(posDataWrapper);
-  let cache = posUserCache.get(userID);
-  if (!cache) {
-    cache = new Set();
-    posUserCache.set(userID, cache);
-  }
-  cache.add(posDataWrapper);
-}
-
-function clearUserBufferAndRedraw(userID) {
-  const userCache = posUserCache.get(userID);
-  if (userCache) {
-    for (const posDataWrapper of userCache) {
-      globalPosBuffer.delete(posDataWrapper);
-    }
-    userCache.clear();
-  }
-  redrawCanvas();
-}
-
-function sendJoinedUserBuffer(targetUserID) {
-  if (posUserCache.has(CURRENT_USER_ID)) {
-    const joinedBuffer = new Array();
-    for (const posDataWrapper of posUserCache.get(CURRENT_USER_ID)) {
-      for (const posData of posDataWrapper) {
-        joinedBuffer.push(-1, ...posData);
-      }
-    }
-    sock.send(new Int32Array(joinedBuffer));
+    mainCanvas.addPosDataToBufferAndDraw(data, userID);
   }
 }
 
@@ -359,24 +125,24 @@ function createPosDataWrapper(posData) {
 
 // ---- Socket ----
 function sendPositions() {
-  if (posBuffer[0] === -2 && posBuffer.length > 2 || posBuffer.length > 4) {
-    const posData = new Int32Array(posBuffer);
+  if (mainCanvas.posBuffer[0] === -2 && mainCanvas.posBuffer.length > 2 || mainCanvas.posBuffer.length > 4) {
+    const posData = new Int32Array(mainCanvas.posBuffer);
     sock.send(posData.buffer);
-    if (posBuffer[0] >= 0) {
-      addPosDataToBuffer(posData, CURRENT_USER_ID);
+    if (mainCanvas.posBuffer[0] >= 0) {
+      mainCanvas.addPosDataToBuffer(posData, CURRENT_USER_ID);
     }
-    resetPosBuffer();
+    mainCanvas.resetPosBuffer();
   }
 }
 // Overrule timer if hue or stroke width has changed
 function sendPositionsIfWidthHasChanged() {
   // NOTE: This assumes that the width stays at position 1 in both normal & erase mode
-  if (widthSlider.value !== posBuffer[1]) {
+  if (widthSlider.value !== mainCanvas.posBuffer[1]) {
     sendPositions();
   }
 }
 function sendPositionsIfHueHasChanged() {
-  if (hueSlider.value !== posBuffer[0]) {
+  if (hueSlider.value !== mainCanvas.posBuffer[0]) {
     sendPositions();
   }
 }
@@ -398,19 +164,19 @@ async function socketReceiveMessage(e) {
       case 'disconnect':
         console.info(data.usr + ' disconnected');
         dispatchNotification(`User #${data.usr} has left the room`)
-        clearUserBufferAndRedraw(data.usr);
-        posUserCache.delete(data.usr);
+        mainCanvas.clearUserBufferAndRedraw(data.usr);
+        mainCanvas.posUserCache.delete(data.usr);
         removeUserFromUsernameList(data.usr);
         break;
       case 'connect':
         console.info(data.usr + ' connected, sending my data');
         dispatchNotification(`User #${data.usr} has entered the room`)
-        sendJoinedUserBuffer();
+        mainCanvas.sendJoinedUserBuffer();
         addUserToUsernameList(data.usr, 'User #' + data.usr);
         break;
       case 'clearUser':
         console.info(data.usr + ' cleared their drawing');
-        clearUserBufferAndRedraw(data.usr);
+        mainCanvas.clearUserBufferAndRedraw(data.usr);
         break;
       case 'changeName':
         changeUsername(data.usr, data.val);
@@ -425,7 +191,7 @@ async function socketReceiveMessage(e) {
   }
 }
 
-// --- Helper functions ---
+// ---- Username handling ----
 function changeOwnUsername(newUsername) {
   if (/^[Uu]ser #\d+$/.test(newUsername)) {
     setOwnDefaultUsername();
@@ -474,17 +240,6 @@ function createUserListNode(username) {
   return node;
 }
 
-function sendMessage(event, value) {
-  const dataObj = {
-    evt: event
-  };
-  if (value != null) {
-    dataObj.val = value;
-  }
-  sock.send(JSON.stringify(dataObj));
-}
-
-
 // ---- Notifications ----
 function dispatchNotification(content) {
   const notification = notificationTemplate.cloneNode(true);
@@ -497,3 +252,19 @@ function dispatchNotification(content) {
     }, 200);
   }, 1600);
 }
+
+// ---- Helper functions ----
+function makeHSLString(hue = hueSlider.value) {
+  return `hsl(${hue}, 75%, 70%)`
+}
+
+function sendMessage(event, value) {
+  const dataObj = {
+    evt: event
+  };
+  if (value != null) {
+    dataObj.val = value;
+  }
+  sock.send(JSON.stringify(dataObj));
+}
+
