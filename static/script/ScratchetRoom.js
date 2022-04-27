2a271357677c3a84d6a54702c38908ac7a19094d
Hallo89 <hallo89@protonmail.com>
2022-04-27 18:30:29 +0200

    Implement basic ScratchetRoom room handling

--- static/script/ScratchetRoom.js
+++ static/script/ScratchetRoom.js
@@ -1,5 +1,38 @@
 class ScratchetRoom extends ScratchetCanvas {
+  roomListNode = ScratchetRoom.createRoomListNode();
+  roomName;
+
   constructor(canvas, roomCode, ownUsername) {
     super(canvas);
+    this.initRoom(roomCode, ownUsername);
+  }
+
+  // ---- Event handling ----
+  roomListNodeClick() {
+    console.log(this.roomName);
+  }
+
+  // ---- Generic room handling ----
+  initRoom(roomCode, ownUsername) {
+    this.changeRoomName(ScratchetRoom.createDefaultName(ownUsername));
+    this.roomListNode.addEventListener('click', this.roomListNodeClick.bind(this));
+    roomList.appendChild(this.roomListNode);
+  }
+
+  changeRoomName(roomName) {
+    this.roomName = roomName;
+    this.roomListNode.textContent = roomName;
+    roomcodeInput.textContent = roomName;
+  }
+
+  // ---- Static helper functions ----
+  static createRoomListNode() {
+    const listNode = document.createElement('span');
+    listNode.classList.add('item');
+    return listNode;
+  }
+
+  static createDefaultName(username) {
+    return username + "'s room";
   }
 }
