81439d1aafa8898cb413e5fd8abdbdff8fd4a66d
Hallo89 <hallo89@protonmail.com>
2022-12-12 23:04:32 +0100

    Move socket buffer functions into `ScratchetBufferController`

--- static/script/ScratchetBufferController.js
+++ static/script/ScratchetBufferController.js
@@ -1,6 +1,109 @@
 'use strict';
 class ScratchtBufferController {
-  constructor() {
+  sendBufferServer = new Array();
+  sendBufferClient = new Array();
 
+  willSendCompleteMetaData = true;
+  /** @type { ScratchetRoom } */
+  activeRoom;
+
+  // ---- Send buffer handling ----
+  addToSendBuffer(posX, posY) {
+    this.sendBufferClient.push(posX, posY);
+    this.sendBufferServer.push(posX, posY);
+  }
+
+  initializeSendBufferNormal(lastPosX, lastPosY) {
+    const hue = this.activeRoom.hue;
+    const width = this.activeRoom.width;
+    let flag = 0;
+
+    this.sendBufferServer = new Array(2);
+
+    if (!this.willSendCompleteMetaData && getClientMetaHue(this.sendBufferClient) === hue) {
+      flag |= 0b0010;
+    } else {
+      this.lastHue = hue;
+      this.sendBufferServer.push(this.lastHue);
+    }
+    if (!this.willSendCompleteMetaData && getClientMetaWidth(this.sendBufferClient) === width) {
+      flag |= 0b0001;
+    } else {
+      this.lastWidth = width;
+      this.sendBufferServer.push(this.lastWidth);
+    }
+
+    this.sendBufferServer.push(lastPosX, lastPosY);
+    this.sendBufferServer[0] = this.activeRoom.roomCode;
+    this.sendBufferServer[1] = flag;
+
+    this.sendBufferClient = [hue, width, lastPosX, lastPosY, flag];
+  }
+  initializeSendBufferErase() {
+    this.sendBufferServer = [this.activeRoom.roomCode, Global.MODE.ERASE, this.activeRoom.width];
+    this.sendBufferClient = [];
+  }
+
+  // TODO this can probably be made less redundant
+  resetSendBuffer() {
+    if (getPendingServerMetaMode(this.sendBufferServer) === Global.MODE.ERASE) {
+      this.initializeSendBufferErase();
+    } else {
+      this.initializeSendBufferNormal(
+        this.sendBufferClient[this.sendBufferClient.length - 2],
+        this.sendBufferClient[this.sendBufferClient.length - 1],
+      );
+    }
+  }
+  // Only update width and hue
+  updateSendBuffer() {
+    if (getPendingServerMetaMode(this.sendBufferServer) === Global.MODE.ERASE) {
+      this.initializeSendBufferErase();
+    } else if (this.sendBufferClient.length > 0) {
+      this.initializeSendBufferNormal(
+        this.sendBufferClient[2],
+        this.sendBufferClient[3],
+      );
+    }
+  }
+
+  // ---- Send handling ----
+  sendPositions() {
+    const mode = getPendingServerMetaMode(this.sendBufferServer);
+
+    if (mode === Global.MODE.ERASE && this.sendBufferServer.length > (META_LEN.ERASE + EXTRA_SERVER_META_LEN)
+        || this.sendBufferClient.length > META_LEN.NORMAL) {
+      const posData = new Int16Array(this.sendBufferServer);
+      sock.send(posData.buffer);
+      if (this.sendBufferClient.length > 0) {
+        this.activeRoom.addClientDataToBuffer(
+          new Int16Array(this.sendBufferClient), this.activeRoom.getOwnUser());
+        // posBufferServer needs to be checked due to asynchronities
+        // between willSendCompleteMetaData and sendPositions
+        // And to ensure that it only resets on normal mode
+        if (this.willSendCompleteMetaData && mode === 0) {
+          this.willSendCompleteMetaData = false;
+        }
+      }
+      this.resetSendBuffer();
+    } else {
+      this.updateSendBuffer();
+    }
+  }
+
+  // Overrule timer if hue or stroke width has changed
+  sendPositionsIfWidthHasChanged() {
+    if (this.activeRoom.width !== getClientMetaWidth(this.sendBufferClient)) {
+      this.sendPositions();
+    }
+  }
+  sendPositionsIfHueHasChanged() {
+    if (this.activeRoom.hue !== getClientMetaHue(this.sendBufferClient)) {
+      this.sendPositions();
+    }
+  }
+
+  sendCompleteMetaDataNextTime() {
+    this.willSendCompleteMetaData = true;
   }
 }
--- static/script/ScratchetController.js
+++ static/script/ScratchetController.js
@@ -1,22 +1,13 @@
 'use strict';
-class ScratchetController {
+class ScratchetController extends ScratchtBufferController {
   globalUsername;
   defaultUsername;
 
-  /**
-   * @type { Map<number, ScratchetRoom> }
-   */
+  /** @type { Map<number, ScratchetRoom> } */
   rooms = new Map();
-  /**
-   * @type { ScratchetRoom }
-   */
-  activeRoom;
-
-  sendBufferServer = new Array();
-  sendBufferClient = new Array();
-  willSendCompleteMetaData = true;
 
   constructor() {
+    super();
     const persistentUsername = localStorage.getItem(LOCALSTORAGE_USERNAME_KEY);
     if (persistentUsername) {
       this.globalUsername = persistentUsername;
@@ -228,110 +219,12 @@ class ScratchetController {
     roomListButton.textContent = this.rooms.size;
   }
 
-  // ---- Canvas handling ----
+  // ---- Canvas helpers ----
   highlightUser(user) {
     this.activeRoom.redrawCanvas(user);
   }
 
-  addToSendBuffer(posX, posY) {
-    this.sendBufferClient.push(posX, posY);
-    this.sendBufferServer.push(posX, posY);
-  }
-
-  initializeSendBufferNormal(lastPosX, lastPosY) {
-    const hue = this.activeRoom.hue;
-    const width = this.activeRoom.width;
-    let flag = 0;
-
-    this.sendBufferServer = new Array(2);
-
-    if (!this.willSendCompleteMetaData && getClientMetaHue(this.sendBufferClient) === hue) {
-      flag |= 0b0010;
-    } else {
-      this.lastHue = hue;
-      this.sendBufferServer.push(this.lastHue);
-    }
-    if (!this.willSendCompleteMetaData && getClientMetaWidth(this.sendBufferClient) === width) {
-      flag |= 0b0001;
-    } else {
-      this.lastWidth = width;
-      this.sendBufferServer.push(this.lastWidth);
-    }
-
-    this.sendBufferServer.push(lastPosX, lastPosY);
-    this.sendBufferServer[0] = this.activeRoom.roomCode;
-    this.sendBufferServer[1] = flag;
-
-    this.sendBufferClient = [hue, width, lastPosX, lastPosY, flag];
-  }
-  initializeSendBufferErase() {
-    this.sendBufferServer = [this.activeRoom.roomCode, Global.MODE.ERASE, this.activeRoom.width];
-    this.sendBufferClient = [];
-  }
-
-  // TODO this can probably be made less redundant
-  resetSendBuffer() {
-    if (getPendingServerMetaMode(this.sendBufferServer) === Global.MODE.ERASE) {
-      this.initializeSendBufferErase();
-    } else {
-      this.initializeSendBufferNormal(
-        this.sendBufferClient[this.sendBufferClient.length - 2],
-        this.sendBufferClient[this.sendBufferClient.length - 1],
-      );
-    }
-  }
-  // Only update width and hue
-  updateSendBuffer() {
-    if (getPendingServerMetaMode(this.sendBufferServer) === Global.MODE.ERASE) {
-      this.initializeSendBufferErase();
-    } else if (this.sendBufferClient.length > 0) {
-      this.initializeSendBufferNormal(
-        this.sendBufferClient[2],
-        this.sendBufferClient[3],
-      );
-    }
-  }
-
-  sendCompleteMetaDataNextTime() {
-    this.willSendCompleteMetaData = true;
-  }
-
-  // ---- Socket handling ----
-  sendPositions() {
-    const mode = getPendingServerMetaMode(this.sendBufferServer);
-
-    if (mode === Global.MODE.ERASE && this.sendBufferServer.length > (META_LEN.ERASE + EXTRA_SERVER_META_LEN)
-        || this.sendBufferClient.length > META_LEN.NORMAL) {
-      const posData = new Int16Array(this.sendBufferServer);
-      sock.send(posData.buffer);
-      if (this.sendBufferClient.length > 0) {
-        this.activeRoom.addClientDataToBuffer(
-          new Int16Array(this.sendBufferClient), this.activeRoom.getOwnUser());
-        // posBufferServer needs to be checked due to asynchronities
-        // between willSendCompleteMetaData and sendPositions
-        // And to ensure that it only resets on normal mode
-        if (this.willSendCompleteMetaData && mode === 0) {
-          this.willSendCompleteMetaData = false;
-        }
-      }
-      this.resetSendBuffer();
-    } else {
-      this.updateSendBuffer();
-    }
-  }
-
-  // Overrule timer if hue or stroke width has changed
-  sendPositionsIfWidthHasChanged() {
-    if (this.activeRoom.width !== getClientMetaWidth(this.sendBufferClient)) {
-      this.sendPositions();
-    }
-  }
-  sendPositionsIfHueHasChanged() {
-    if (this.activeRoom.hue !== getClientMetaHue(this.sendBufferClient)) {
-      this.sendPositions();
-    }
-  }
-
+  // ---- Socket receive handling ----
   parseSocketData(data) {
     const mode = getReceivedServerMetaMode(data);
     const userID = data[0];
