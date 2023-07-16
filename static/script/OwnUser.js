d523c31ed0e50fd0e180f692310f26f40aa57d99
Hallo89 <hallo89@protonmail.com>
2023-07-16 17:29:15 +0200

    Add undo brush group handling to `OwnUser`, tracking the undo count

--- static/script/OwnUser.js
+++ static/script/OwnUser.js
@@ -1,4 +1,21 @@
 'use strict';
 class OwnUser extends ScratchetUser {
+  brushUndo = {
+    groups: [],
+    groupIndex: 0
+  };
 
+
+  // ---- Undo Brush grouping ----
+  addBrushUndoGroup(initialLength) {
+    const count = this.posCache.length - initialLength;
+    if (count > 0) {
+      this.brushUndo.groups.push(count);
+      this.brushUndo.groupIndex++;
+    }
+  }
+
+  getNextBrushUndoGroup() {
+    return this.brushUndo.groups[this.brushUndo.groupIndex];
+  }
 }
