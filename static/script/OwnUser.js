95f041356156f47b142ce5dced7e9651a417b8f7
Hallo89 <hallo89@protonmail.com>
2023-07-17 21:37:14 +0200

    `OwnUser`: Make undo grouping work for any buffer

--- static/script/OwnUser.js
+++ static/script/OwnUser.js
@@ -1,26 +1,39 @@
 'use strict';
 class OwnUser extends ScratchetUser {
-  brushUndo = {
+  undoData = {
     groups: [],
     groupIndex: 0
   };
 
+  /** @type { Array } */
+  #captureBuffer;
+  /** @type { number } */
+  #captureStartLen;
+
 
   clearRedoBuffer() {
-    this.brushUndo.groups.splice(this.brushUndo.groupIndex);
+    this.undoData.groups.splice(this.undoData.groupIndex);
     super.clearRedoBuffer();
   }
 
-  // ---- Undo Brush grouping ----
-  addBrushUndoGroup(initialLength) {
-    const count = this.posCache.length - initialLength;
+  // ---- Undo data grouping ----
+  startUndoGroupCapture(buffer) {
+    this.#captureBuffer = buffer;
+    this.#captureStartLen = buffer.length;
+  }
+  captureUndoGroup() {
+    if (!this.#captureBuffer) return;
+
+    const count = this.#captureBuffer.length - this.#captureStartLen;
     if (count > 0) {
-      this.brushUndo.groups.push(count);
-      this.brushUndo.groupIndex++;
+      this.undoData.groups.push(count);
+      this.undoData.groupIndex++;
     }
+    this.#captureBuffer = null;
+    this.#captureStartLen = null;
   }
 
-  getNextBrushUndoGroup() {
-    return this.brushUndo.groups[this.brushUndo.groupIndex];
+  getNextUndoGroup() {
+    return this.undoData.groups[this.undoData.groupIndex];
   }
 }
