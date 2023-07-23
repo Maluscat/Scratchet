aa801790fc241e1e4c53fd649ff7d60e7516399c
Hallo89 <hallo89@protonmail.com>
2023-07-23 02:59:27 +0200

    p2: Move canvas redraw into `CanvasView`

--- static/script/ScratchetCanvas.js
+++ static/script/ScratchetCanvas.js
@@ -188,68 +188,6 @@ class ScratchetCanvas extends CanvasViewTransform {
     user.redo(this, count);
   }
 
-  // ---- Canvas handling ----
-  /** @param {ScratchetUser} [userHighlight] */
-  redrawCanvas(userHighlight) {
-    // TODO skip unseen points
-    let prevPosData;
-    let prevPosDataWrapper;
-
-    this.ctx.clearRect(0, 0, CanvasViewTransform.VIEW_WIDTH, CanvasViewTransform.VIEW_HEIGHT);
-
-    for (const { posData, wrapperStack } of ScratchetCanvas.iteratePosWrapper(this.posBuffer)) {
-      let isFromHighlightedUser = false;
-
-      if (userHighlight != null) {
-        isFromHighlightedUser = !userHighlight.posCache.includes(wrapperStack[1]);
-      }
-      if (!prevPosData
-          || getClientMetaHue(posData) !== getClientMetaHue(prevPosData)
-          || getClientMetaWidth(posData) !== getClientMetaWidth(prevPosData)
-            /* This forces a stroke when changing from one user to another with highlight enabled */
-          || userHighlight != null && (!userHighlight.posCache.includes(prevPosDataWrapper) !== isFromHighlightedUser)) {
-
-        if (prevPosData) {
-          this.ctx.stroke();
-        }
-
-        // ASSUMPTION: all posData in posDataWrapper have the same width and hue
-        // because only the eraser can form multiple posData inside one wrapper
-        this.setStrokeStyle(getClientMetaHue(posData), isFromHighlightedUser);
-        this.setLineWidth(getClientMetaWidth(posData));
-
-        this.ctx.beginPath();
-      }
-
-      this.drawFromPosData(posData);
-
-      prevPosData = posData;
-      if (wrapperStack[1] !== prevPosDataWrapper) {
-        prevPosDataWrapper = wrapperStack[1];
-      }
-    }
-
-    if (prevPosData) {
-      this.ctx.stroke();
-
-      this.setStrokeStyle();
-      this.setLineWidth();
-    }
-  }
-
-  drawFromPosData(posData) {
-    this.ctx.moveTo(posData[META_LEN.BRUSH], posData[META_LEN.BRUSH + 1]);
-    let i = META_LEN.BRUSH;
-    for (; i < posData.length - 4; i += 6) {
-      this.ctx.bezierCurveTo(posData[i], posData[i + 1], posData[i + 2], posData[i + 3], posData[i + 4], posData[i + 5]);
-    }
-    // Draw the finishing points of the wrapper in case the wrapper hasn't fully been drawn above
-    if (i === posData.length - 4) {
-      this.ctx.quadraticCurveTo(posData[i], posData[i + 1], posData[i + 2], posData[i + 3]);
-    } else if (i === posData.length - 2) {
-      this.ctx.lineTo(posData[i], posData[i + 1]);
-    }
-  }
 
   // ---- Pos buffer ----
   setLastPos(posX, posY) {
@@ -441,15 +379,6 @@ class ScratchetCanvas extends CanvasViewTransform {
 
   deleteFromPosBuffer(item) {
     this.posBuffer.splice(this.posBuffer.indexOf(item), 1);
-  }
-
-
-  // ---- Misc helper functions ----
-  setLineWidth(width = this.tools.brush.width) {
-    this.ctx.lineWidth = width;
-  }
-  setStrokeStyle(hue = this.tools.brush.hue, hasReducedAlpha) {
-    this.ctx.strokeStyle = makeHSLString(hue, hasReducedAlpha);
   }
 
 
