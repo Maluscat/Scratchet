ac6ea414b57ab1e1291e28691b8327bd4171e211
Hallo89 <hallo89@protonmail.com>
2023-09-15 00:48:56 +0200

    Add a simple worker scaffolding, setup offscreenCanvas & ctx on init

--- static/script/worker/drawHandler.worker.js
+++ static/script/worker/drawHandler.worker.js
@@ -1,1 +1,33 @@
 'use strict';
+/**
+ * @typedef { Object } ScratchetMessageEvent
+ * @prop { string } event
+ * @prop { any } [values]
+ */
+
+/** @type { OffscreenCanvas } */
+let offscreenCanvas;
+/** @type { OffscreenCanvasRenderingContext2D } */
+let ctx;
+
+
+self.onmessage = e => {
+  if (typeof e.data === 'object') {
+
+    /** @type { ScratchetMessageEvent } */
+    const { event, values } = e.data;
+
+    if (event in this) {
+      // Call the given event as function
+      this[event](values);
+    } else {
+      console.error(`No corresponding function for event ${event} found.`);
+    }
+  }
+};
+
+
+function init({ canvas }) {
+  offscreenCanvas = canvas;
+  ctx = canvas.getContext('2d');
+}
