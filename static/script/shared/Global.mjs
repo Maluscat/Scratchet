4b5df67f5cd45f7a835c93107985390978e11726
Hallo89 <hallo89@protonmail.com>
2023-09-15 18:15:15 +0200

    p1: Split `Validator` from `Global`, put both into ./shared/
    + Change how their exports work

---
+++ static/script/shared/Global.mjs
@@ -0,0 +1,9 @@
+// ---- Constants ----
+export const MAX_INIT_TRANSMISSION_TIME = 30 * 1000;
+export const SEND_INTERVAL = 40;
+export const MODE = {
+  BULK_INIT: -1,
+  ERASE: -2,
+  UNDO: -3,
+  REDO: -4,
+};
---
+++ static/script/shared/Validator.mjs
@@ -0,0 +1,35 @@
+'use strict';
+// ---- Constants ----
+export const MAX_USERNAME_LENGTH = 20;
+// The additional 7 characters is the length of "'s room"
+export const MAX_ROOM_NAME_LENGTH = MAX_USERNAME_LENGTH + 7;
+
+export const JOINROOM_VALIDATE_REGEX = /^(?:(?:https?:\/\/)?\w+(?:\.\w+)*(?::\d{1;5})?(?:\/\w*?)*#)?(\d{4})$/;
+
+// ---- Functions ----
+export function validateAndReturnRoomCode(roomCode) {
+  roomCode = parseInt(roomCode);
+  if (!Number.isNaN(roomCode) && roomCode >= 1 && roomCode <= 9999) {
+    return roomCode;
+  }
+  return false;
+}
+
+export function validateRoomInputValueToRoomCode(roomInputValue) {
+  const match = roomInputValue.match(JOINROOM_VALIDATE_REGEX);
+  return validateAndReturnRoomCode(match?.[1]);
+}
+
+export function validateRoomName(roomName) {
+  if (!roomName || roomName.length > MAX_ROOM_NAME_LENGTH) {
+    return false;
+  }
+  return true;
+}
+
+export function validateUsername(username) {
+  if (!username || /^[Uu]ser #\d+$/.test(username) || username.length > MAX_USERNAME_LENGTH) {
+    return false
+  }
+  return true;
+}
