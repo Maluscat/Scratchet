dbcb11c9ccb6d55c8073e0b9e18d4f337cf817ea
Hallo89 <hallo89@protonmail.com>
2023-09-19 18:44:36 +0200

    Add a `view` instance in the draw worker, add worker events

--- static/script/worker/drawHandler.worker.js
+++ static/script/worker/drawHandler.worker.js
@@ -5,10 +5,6 @@
  * @prop { any } [values]
  */
 
-/** @type { OffscreenCanvas } */
-let offscreenCanvas;
-/** @type { OffscreenCanvasRenderingContext2D } */
-let ctx;
 importScripts(
   './CanvasDraw.worker.js',
   './CanvasView.worker.js',
@@ -16,24 +12,41 @@ importScripts(
   '../PositionDataHandler.js',
 );
 
+/** @type { CanvasViewWorker } */
+let view;
+
 
 self.onmessage = e => {
-  if (typeof e.data === 'object') {
+  /** @type { ScratchetMessageEvent } */
+  const { event, values } = e.data;
 
-    /** @type { ScratchetMessageEvent } */
-    const { event, values } = e.data;
-
-    if (event in this) {
-      // Call the given event as function
-      this[event](values);
-    } else {
-      console.error(`No corresponding function for event ${event} found.`);
-    }
+  if (event in this) {
+    // Call the given event as function
+    this[event](values);
+  } else {
+    console.error(`No corresponding function for event ${event} found.`);
   }
 };
 
 
-function init({ canvas }) {
-  offscreenCanvas = canvas;
-  ctx = canvas.getContext('2d');
+function init({ canvas, dpr, width, height }) {
+  view = new CanvasViewWorker(canvas);
+  view.setDimensions(width, height);
+  view.setDevicePixelRatio(dpr);
+}
+
+function update({ buffers }) {
+  view.update(buffers);
+}
+
+function setDimensions({ width, height }) {
+  view.setDimensions(width, height);
+}
+// TODO: There is no update event yet to send this event on dpr change
+function setDevicePixelRatio({ dpr }) {
+  view.setDevicePixelRatio(dpr);
+}
+
+function setTransform(args) {
+  view.setTransform(...args);
 }
