5d9b58c46dae055dab68dea6c545dc4a7a37646c
Hallo89 <hallo89@protonmail.com>
2024-02-28 00:01:35 +0100

    `Global`: Add flags for bulk init brush undo/redo

--- static/script/shared/Global.mjs
+++ static/script/shared/Global.mjs
@@ -1,10 +1,12 @@
 // ---- Constants ----
 export const MAX_INIT_TRANSMISSION_TIME = 30 * 1000;
 export const SEND_INTERVAL = 40;
-export const MODE = {
+export const MODE = /** @type const */ ({
   BULK_INIT: -1,
   ERASE: -2,
   UNDO: -3,
   REDO: -4,
   HISTORY_MARKER: -5,
-};
+  BULK_INIT_BRUSH_UNDO: -10,
+  BULK_INIT_BRUSH_REDO: -11,
+});
