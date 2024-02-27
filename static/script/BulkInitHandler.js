535cba696ddd95f985f6848372ad276161889a2d
Hallo89 <hallo89@protonmail.com>
2024-02-28 00:02:00 +0100

    p1: Refactor `BulkInitHandler` to accomodate for brush history

--- static/script/BulkInitHandler.js
+++ static/script/BulkInitHandler.js
@@ -1,18 +1,54 @@
 'use strict';
 class BulkInitHandler {
+  #brushRedoCount = 0;
+
+
+  /**
+   * @param { Array } data
+   * @param { ScratchetUser } user
+   */
   receive(data, user) {
-    let startIndex = BULK_INIT_SEPARATOR_LEN;
-    for (let i = startIndex; i < data.length; i++) {
-      if (data[i] === Global.MODE.BULK_INIT) {
-        this.#sliceInitDataAndAddToBuffer(data, user, startIndex, i);
-        startIndex = i + BULK_INIT_SEPARATOR_LEN;
+    let startIndex = 1;
+    let mode = Global.MODE.BULK_INIT;
+    let i = startIndex;
+
+    for (; i < data.length; i++) {
+      if (data[i] < 0) {
+        this.bulkInitLoop(mode, data, user, startIndex, i);
+        startIndex = i + 1;
+        mode = data[i];
       }
     }
-    this.#sliceInitDataAndAddToBuffer(data, user, startIndex);
+    this.bulkInitLoop(mode, data, user, startIndex, i);
+    user.historyHandler.undo(this.#brushRedoCount);
+    this.#brushRedoCount = 0;
   }
-  #sliceInitDataAndAddToBuffer(data, user, startIndex, endIndex = Infinity) {
-    const posData = data.subarray(startIndex, endIndex);
-    const wrapperDestIndex = data[startIndex - BULK_INIT_SEPARATOR_LEN + 1];
-    user.addServerDataToBuffer(posData, wrapperDestIndex);
+  bulkInitLoop(mode, data, user, startIndex, i) {
+    switch (mode) {
+      case Global.MODE.BULK_INIT_BRUSH_REDO:
+        this.#incrementBrushRedo(data, user);
+      case Global.MODE.BULK_INIT_BRUSH_UNDO:
+        this.#addBrushGroup(data, user);
+        break;
+      default:
+        this.#addPosData(data, user, startIndex, i);
+    }
+  }
+
+  #addBrushGroup(data, user) {
+    user.historyHandler.addGroup();
+  }
+  #incrementBrushRedo(data, user) {
+    this.#brushRedoCount++;
+  }
+  #addPosData(data, user, startIndex, endIndex) {
+    const wrapperDestIndex = data[startIndex];
+    startIndex += BULK_INIT_SEPARATOR_LEN - 1;
+    if (startIndex === endIndex) {
+      user.addClientDataToBuffer(false, wrapperDestIndex);
+    } else {
+      const posData = data.subarray(startIndex, endIndex);
+      user.addServerDataToBuffer(posData, wrapperDestIndex);
+    }
   }
 }
