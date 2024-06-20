3e2d800897053e1dd7d514b9cb5948975e75b403
Malus <hallo89@protonmail.com>
2024-06-20 23:01:52 +0200

    HistoryGroup: Add `equal` method to compare groups

--- static/script/history/HistoryGroup.js
+++ static/script/history/HistoryGroup.js
@@ -1,3 +1,5 @@
+import { PositionDataHandler } from '~/PositionDataHandler.js';
+
 /** @typedef { import('~/user/User.js').User } User */
 
 /**
@@ -9,7 +11,7 @@ export class HistoryGroup {
    * Specifies the amount of pings that have been dispatched
    * while this group was being constructed.
    */
-  intactCount = 0;
+  intactCount = null;
 
   undo() {
     throw new Error('You have to implement this method!');
@@ -25,5 +27,24 @@ export class HistoryGroup {
   /** @param { number } intactCount */
   close(intactCount) {
     this.intactCount = intactCount;
+  }
+
+  /** @param { HistoryGroup } group */
+  equal(group) {
+    if (group.historyData.length !== this.historyData.length) return false;
+
+    for (let i = 0; i < this.historyData.length; i++) {
+      const data = this.historyData[i];
+      const dataTarget = group.historyData[i];
+      if (data.posWrapper.length !== dataTarget.posWrapper.length) return false;
+
+      for (let j = 0; j < data.posWrapper.length; j++) {
+        const posWrapper = data.posWrapper[j]
+        const posWrapperTarget = dataTarget.posWrapper[j];
+        if (!PositionDataHandler.posDataIsEqual(posWrapper, posWrapperTarget)) return false;
+      }
+    }
+
+    return true;
   }
 }
